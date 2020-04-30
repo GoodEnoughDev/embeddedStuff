41b05434a7e1170ee0f00588c60b809ef10def3f
Andrew Bingham <bingham.andrew.34@gmail.com>
2020-04-30 11:29:56 -0700

    Command parser task working

--- my-project/FreeRTOSConfig.h
+++ my-project/FreeRTOSConfig.h
@@ -115,20 +115,26 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil 			0
 #define INCLUDE_vTaskDelay 					1
 
-/* This is the raw value as per the Cortex-M3 NVIC.  Values can be 255
-(lowest) to 0 (1?) (highest). */
-#define configKERNEL_INTERRUPT_PRIORITY 255
+/* Cortex-M specific definitions. */
+#define configPRIO_BITS         4
+
+/* The lowest interrupt priority that can be used in a call to a "set priority"
+function. */
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY   15
+
+/* The highest interrupt priority that can be used by any interrupt service
+routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
+INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
+PRIORITY THAN THIS! (higher priorities are lower numeric values. */
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
+
+/* Interrupt priorities used by the kernel port layer itself.  These are generic
+to all Cortex-M ports, and do not rely on any particular library functions. */
+#define configKERNEL_INTERRUPT_PRIORITY         ( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 -     configPRIO_BITS) )
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY \
-  191 /* equivalent to 0xb0, or priority 11. */
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    ( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY <<     (8 - configPRIO_BITS) )
 
-/* This is the value being used as per the ST library which permits 16
-priority values, 0 to 15.  This must correspond to the
-configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
-NVIC value of 255. */
-#define configLIBRARY_KERNEL_INTERRUPT_PRIORITY 15
-
-#define configASSERT( x ) if ((x) == 0) {taskDISABLE_INTERRUPTS(); for( ;; );}
+//#define configASSERT( x ) if ((x) == 0) {taskDISABLE_INTERRUPTS(); for( ;; );}
 
 #endif /* FREERTOS_CONFIG_H */
--- my-project/my-project.c
+++ my-project/my-project.c
@@ -8,10 +8,10 @@
 #include "task.h"
 #include "printf.h"
 #include "queue.h"
+#include "semphr.h"
 
-
-#define COMMAND_BUFFER_LEN 100
-#define COMMAND_QUEUE_LEN 142
+#define COMMAND_QUEUE_LEN 20
+#define COMMAND_BUFFER_LEN 50
 
 xQueueHandle command_queue;
 xSemaphoreHandle print_semaphore;
@@ -70,12 +70,11 @@ static void gpio_setup(void)
 void testTask(void)
 {
 	xSemaphoreTake(print_semaphore, portMAX_DELAY);
-	printf("Starting testTask");
+	printf("Starting testTask\r\n");
 	xSemaphoreGive(print_semaphore);
 	const TickType_t delay = pdMS_TO_TICKS(25);
 	while(1)
 	{
-			gpio_toggle(GPIOB, GPIO0);
 			vTaskDelay(delay);
 	}
 }
@@ -83,7 +82,7 @@ void testTask(void)
 void heap_monitor_task(void)
 {
 	xSemaphoreTake(print_semaphore, portMAX_DELAY);
-    printf("Started heap monitor task");
+    printf("Started heap monitor task\r\n");
 	xSemaphoreGive(print_semaphore);
     uint32_t current_heap_size;
     const TickType_t delay = pdMS_TO_TICKS(500);
@@ -100,10 +99,43 @@ void heap_monitor_task(void)
 	}
 }
 
+void command_parser_task(void)
+{
+	xSemaphoreTake(print_semaphore, portMAX_DELAY);
+    printf("Started command parser task\r\n");
+	xSemaphoreGive(print_semaphore);
+	char character;
+	char command_buffer[COMMAND_BUFFER_LEN];
+	int index = 0;
+	const TickType_t delay = pdMS_TO_TICKS(25);
+
+	while(1)
+	{
+		xQueueReceive(command_queue, &character, portMAX_DELAY);
+		if(character != NULL)
+		{
+			if(character == '\n' || character == '\r')
+			{
+				for(int i = 0; i <= index; i++)
+				{
+					usart_send_blocking(USART3, command_buffer[i]);
+					command_buffer[i] = 0;
+					//vTaskDelay(delay);
+				}
+				index = 0;
+				printf("\r\n");
+			}
+			command_buffer[index] = character;
+			gpio_toggle(GPIOB, GPIO0);
+			index++;
+		}
+	}
+}
+
 int main(void) 
 {
 	uint32_t rc = 0;
-	print_semaphore = xSemaphoreCreateMutex(void);
+	print_semaphore = xSemaphoreCreateMutex();
 	clock_setup();
 	gpio_setup();
 	usart_setup();
@@ -119,6 +151,9 @@ int main(void)
 	command_queue = xQueueCreate(COMMAND_QUEUE_LEN, sizeof(char));	
 	printf("Command queue created with handle: %d\r\n");
 
+	// Create command parser task
+	xTaskCreate(command_parser_task, "command_parser_task", 200, NULL, configMAX_PRIORITIES-1, NULL);
+
 	vTaskStartScheduler();
 	printf("vTaskStartScheduler returned");
 	return 0;
@@ -126,16 +161,19 @@ int main(void)
 
 void usart3_isr(void)
 {
+	BaseType_t xHighPriorityTaskWoken;
+	xHighPriorityTaskWoken = pdFALSE;
+	static char character;
 	/* Check if we were called because of RXNE. */
 	if (((USART_CR1(USART3) & USART_CR1_RXNEIE) != 0) &&
 	    ((USART_SR(USART3) & USART_SR_RXNE) != 0)) 
 	{
 		/* Indicate that we got data. */
-		gpio_toggle(GPIOB, GPIO0);
+		//gpio_toggle(GPIOB, GPIO0);
 
 		/* Retrieve the data from the peripheral. */
-		char character = usart_recv(USART3);
-		xQueueSendToBackFromISR(command_queue, &character, pdFALSE);
-
+		character = usart_recv(USART3);
+		
+		xQueueSendToBack(command_queue, &character, xHighPriorityTaskWoken);
 	}
 }
